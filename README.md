75b2c2150d78c9fe33debfa2169890e65b27f8b6
Cosmos-12 <maelsalem@outlook.com>
2025-01-27 09:54:21 +0100

    acceuil changé, page skill avancée, font-size topnav ajustée, styles globaux optimisés

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # NG19_MS
-- début du projet : fin Décembre 2024
-> Ce projet est développé en utilisant la version 19 d'Angular.
+
+> début du projet : fin Décembre 2024
+
+Ce projet est développé en utilisant la version 19 d'Angular.
 
 ## But
-> Montrer l'évolution de mes compétences aussi bien avec le contenu de mon portfolio qu'avec sa forme.
+
+Montrer l'évolution de mes compétences aussi bien avec le contenu de mon portfolio qu'avec sa forme.
 
 ## Interface, design
-L'interface a été pensée pour être simple et élégante, créative et professionnelle, dans le but de refléter un maximum la personnalité de son créateur.
 
+L'interface a été pensée pour être simple et élégante, créative et professionnelle, dans le but de refléter un maximum la personnalité de son créateur.
--- src/tech-tray/tech-tray.component.ts
+++ src/tech-tray/tech-tray.component.ts
@@ -15,8 +15,8 @@ interface skillContent {
 })
 export class TechTrayComponent {
   skills: Array<skillContent> = [
-    { name: "Java", desc: "un des pioniers de l'OOP, toujours en constante mise à jour" },
-    { name: "Angular", desc: "ce portfolio est lui même codé en Angular !" },
-    { name: "C++", desc: "actuellement en train d'apprendre QT !" }
+    { name: "Java", desc: "un des pioniers de l'OOP, toujours en constante mise à jour", iconLink: "https://www.minecraftkrant.nl/media/166/java-logo.png" },
+    { name: "Angular", desc: "ce portfolio est lui même codé en Angular !", iconLink: "https://miro.medium.com/v2/resize:fit:1200/1*Klh1l7wkoG6PDPb9A5oCHQ.png" },
+    { name: "C++", desc: "actuellement en train d'apprendre QT !", iconLink: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/459px-ISO_C%2B%2B_Logo.svg.png?20170928190710" }
   ]
 }
